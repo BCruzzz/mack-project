adc39fed2207c6ead04cda4f4ca4bffbe12576fe
BCruzzz <bmcruzz@hotmail.com>
2024-05-01 21:45:07 -0300

    iteracao2 first commit

---
+++ script_turma.js
@@ -0,0 +1,6 @@
+// Aplicação versão 2.0.0 01/05/2024
+
+function selecionarTurma(turma) {
+    alert("Turma " + turma + " selecionada!");
+    // Aqui você pode adicionar a lógica para fazer algo com a turma selecionada, como redirecionar para outra página, etc.
+}
